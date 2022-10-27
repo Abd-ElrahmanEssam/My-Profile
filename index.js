480a1031ed148d4ce0cc0a93cf31b6663ec28994
Abd-ElrahmanEssam <104840167+Abd-ElrahmanEssam@users.noreply.github.com>
2022-10-27 11:04:13 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+// alert("Welcome to my site");
+// prompt("Welcome to my site");
+
+// onload=alert("Hello");
+var name1= onload=prompt("Write your name please");
+onload=alert("Welcome to my site Mr./Miss."+name1);
